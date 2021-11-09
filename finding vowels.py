dcdc551911d31ee4d750fb6d7bd15fc5fa189b6b
Rahul1234567rr <92287609+Rahul1234567rr@users.noreply.github.com>
2021-11-09 19:48:35 +0530

    Add files via upload

---
+++ finding vowels.py
@@ -0,0 +1,5 @@
+a = input("Enter an alphabet:")
+if a=='a' or a=='A' or a=='e' or a=='E' or a=='i' or a=='I' or a=='o' or a=='O' or a=='u' or a=='U':
+    print(a, "is an vowel")
+else:
+    print(a, "is a consonant")
